ad4db6070a949d38de4e19c83e7c6336973ba542
Inconsumable <inconsumable1812@gmail.com>
2022-08-12 13:26:03 +0800

    del unused

--- src/components/SettingsItem.vue
+++ src/components/SettingsItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="item" ref="itemDOM">
+  <div class="item">
     <div class="item__title">
       <div class="burger handle item__icon">
         <BurgerIcon />
@@ -11,7 +11,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, Item, ref } from 'vue';
+import { defineComponent, Item } from 'vue';
 import TrashIcon from './Icon/TrashIcon.vue';
 import BurgerIcon from './Icon/BurgerIcon.vue';
 import { key } from '@/store';
@@ -22,14 +22,6 @@ export default defineComponent({
     const store = useStore(key);
     const items = store.getters.getItems as Item[];
     const currentItem = items.find((item) => item.id === props.id);
-    const itemDOM = ref<null | HTMLDivElement>(null);
-
-    //  draggable="true"
-    // :ondragstart="handlerDragStart"
-    // :ondragleave="handlerDragLeave"
-    // :ondragend="handlerDragEnd"
-    // :ondragover="handlerDragOver"
-    // :ondrop="handlerDrop"
 
     const removeItem = () => {
       if (currentItem === undefined) return;
@@ -37,47 +29,8 @@ export default defineComponent({
       store.commit('deleteItem', props.id);
     };
 
-    const handlerDragStart = () => {
-      if (currentItem === undefined) return;
-
-      store.commit('changeCurrentItem', currentItem);
-    };
-
-    const handlerDragLeave = () => {
-      if (itemDOM.value === null) return;
-      itemDOM.value.style.background = '#eee';
-    };
-
-    const handlerDragEnd = () => {
-      if (itemDOM.value === null) return;
-      itemDOM.value.style.background = '#eee';
-    };
-
-    const handlerDragOver = (e: Event) => {
-      e.preventDefault();
-      if (itemDOM.value === null) return;
-
-      itemDOM.value.style.background = 'rgb(159, 159, 159)';
-    };
-
-    const handlerDrop = (e: Event) => {
-      e.preventDefault();
-      if (itemDOM.value === null) return;
-      if (currentItem === undefined) return;
-
-      store.commit('changeOrder', currentItem);
-      store.commit('sortItems');
-      itemDOM.value.style.background = '#eee';
-    };
-
     return {
-      removeItem,
-      handlerDragStart,
-      handlerDragLeave,
-      handlerDragOver,
-      handlerDragEnd,
-      handlerDrop,
-      itemDOM
+      removeItem
     };
   },
   components: {

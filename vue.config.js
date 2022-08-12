65cdad5ad34d6f59de6e9507a076605757c850b9
Inconsumable <inconsumable1812@gmail.com>
2022-08-12 13:39:07 +0800

    change publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/weather-widget/' : '/',
   configureWebpack: {
     // No need for splitting
     optimization: {
